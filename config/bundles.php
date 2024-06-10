dcd30d63716a2a84d88e6eb73de03e8a1855b17f
marcmedhat6211 <marcmedhat6211@gmail.com>
2024-06-10 20:01:04 +0300

    downloaded a bundle for handling pagination

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
